70797882dc04c3706a555f1cbce1439302cc4978
Shaikh Saqlain <saqlainsk18@gmail.com>
2021-12-14 17:17:41 +0530

    Create FileReader.java

---
+++ DAY 4/FileReader.java
@@ -0,0 +1,23 @@
+package P4;
+
+import java.nio.file.*;
+import java.io.IOException;
+
+public class FileReader {
+
+	public static void main(String[] args) {
+
+		String file = "C:\\Users\\saqla\\Desktop\\";
+
+		try {
+			System.out.println(read(file));
+		} catch (Exception o) {
+			System.out.println("File not found");
+		}
+	}
+
+	public static String read(String file) throws IOException {
+		return new String(Files.readAllBytes(Paths.get(file))); // Returning the String object containing the contents of the file
+	}
+
+}
